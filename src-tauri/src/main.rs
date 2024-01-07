315f871be52584f76d735fec3e35f5a76ca2f76b
YvesStraten <yves.straten@gmail.com>
2024-01-07 19:19:10 +0800

    Init 1.0.0

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+use std::path::Path;
 use std::process::Command;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -9,17 +11,19 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn download(link: &str, case: u32) -> String {
+#[tauri::command(async)]
+fn download(link: &str, case: u32, path: &str) -> String {
     if case == 0 {
         Command::new("spotdl")
             .arg(&link)
-            .status()
+            .current_dir(&path)
+            .output()
             .expect("did not work");
     } else {
         Command::new("yt-dlp")
             .arg(&link)
-            .status()
+            .current_dir(&path)
+            .output()
             .expect("did not work");
     }
 
@@ -27,6 +31,7 @@ fn download(link: &str, case: u32) -> String {
 }
 
 fn main() {
+    let _ = fix_path_env::fix();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![download])
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,65 +1,88 @@
 import { invoke } from "@tauri-apps/api";
+import { isPermissionGranted, requestPermission, sendNotification } from "@tauri-apps/api/notification";
+import { downloadDir } from "@tauri-apps/api/path";
 import { useState } from "react";
 
-const Form = ({ Links, setLinks, mode}) => {
-const [input, setInput ] = useState("")
-
-const updateInput = (event) => {
-console.log(event);
-setInput(event);
+type formType = {
+	Links: String[],
+	setLinks: (args: any) => void,
+	mode: String
 }
+const Form = ({ Links, setLinks, mode }: formType) => {
+	const [input, setInput] = useState<String>("")
 
-const updateLinks = () => {
-setLinks([...Links, input])
-console.log(Links);
-}
+	const updateInput = (event: String) => {
+		console.log(event);
+		setInput(event);
+	}
 
-const deleteItem = (link) => {
-setLinks(Links.filter(item => item !== link))
+	const updateLinks = () => {
+		setLinks([...Links, input])
+		console.log(Links);
+		setInput("")
+	}
 
-}
+	const deleteItem = (link: String) => {
+		setLinks(Links.filter((item: String) => item !== link))
 
-const download = () => {
-console.log(mode);
-for (let i = 0; i <= Links.length -1; i++) {
-if(mode === "spot" ){
-invoke('download', { link: `${Links[i]}`, case: 0})
-  // `invoke` returns a Promise
-  .then((response) => console.log(response))
-} else {
+	}
 
-invoke('download', { link: `${Links[i]}`, case: 1})
-  // `invoke` returns a Promise
-  .then((response) => console.log(response))
-}
-}
+	const sendNotif = async (currentItem: number, allItems: number) => {
+		let permissionGranted = await isPermissionGranted();
+		if (!permissionGranted) {
+			const permission = await requestPermission();
+			permissionGranted = permission === 'granted';
+		}
+		if (permissionGranted) {
+			sendNotification({ title: 'Download progress', body: `Download ${currentItem}/${allItems}` });
+		}
+	}
 
-}
+	const download = async () => {
+		console.log(mode);
+		const dir = await downloadDir();
+		console.log(dir);
+		for (let i = 0; i <= Links.length - 1; i++) {
+			if (mode === "spot") {
+				invoke('download', { link: `${Links[i]}`, case: 0, path: `${dir}` })
+					.then((response) => {
+						console.log(response)
+						sendNotif(i + 1, Links.length)
+					})
+			} else {
 
-						/* TODO: add a way to delete links
-						 */
-/* 						TODO: add a way to extract audio with yt-dlp */
-return ( 
-<>
-						<input type="text" placeholder="Link to playlist or song" onChange={(e) => updateInput(e.target.value)}></input>
-						<button onClick={() => updateLinks()}>Submit</button>
+				invoke('download', { link: `${Links[i]}`, case: 1, path: `${dir}` })
+					// `invoke` returns a Promise
+					.then((response) => {
+						console.log(response)
+						sendNotif(i + 1, Links.length)
+					})
+			}
+		}
 
+	}
 
-						<h1>Queue</h1>
-						<ol>
-						{Links.map(link => (
-						<>
+	/* 						TODO: add a way to extract audio with yt-dlp */
+	return (
+		<>
+			<input type="text" placeholder="Link to playlist or song" onChange={(e) => updateInput(e.target.value)}></input>
+			<button onClick={() => updateLinks()}>Submit</button>
+
+			<h1>Queue</h1>
+			<ol>
+				{Links.map((link: any) => (
+					<>
 						<li key={link}>{link}</li>
 						<button key={link} onClick={() => deleteItem(link)}>X</button>
-						</>)
-						)}
+					</>)
+				)}
 
-						</ol>
-					<button onClick={() => download()}>Download!</button>
-					<button onClick={() => setLinks([])}>Clear queue!</button>
+			</ol>
+			<button onClick={() => download()}>Download!</button>
+			<button onClick={() => setLinks([])}>Clear queue!</button>
 
-					</>
-)
+		</>
+	)
 }
 
 export default Form;
